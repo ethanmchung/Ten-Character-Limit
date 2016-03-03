2bcf3912076340223b175bd26741cdf100bf860e
ethan <echung2018@pingry.org>
2016-03-03 11:05:28 -0500

    added a comment

--- tenCharacterLimitHW.py
+++ tenCharacterLimitHW.py
@@ -1,13 +1,16 @@
-f = open("input.txt","r+")
+f = open("input.txt","r")
 fo = open("output.txt","w")
 
 eachLines = f.readlines()
+f.close()
 
 for line in eachLines:
 	actual = len(line)
-	leftOver = actual - 10
+	leftOver = actual - 10 #leftOver is the left over characters that exceed the limited 10
 	if actual > 10:
 		line = line[:-leftOver]
 		fo.write(line + "\n")
 	else:
 		fo.write(line)
+
+fo.close()
